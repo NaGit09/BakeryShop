ca09526fce51dd420fcd84f3be129d32709730f7
NaGit09 <nhutanhtran806@gmail.com>
2024-11-18 13:46:58 +0700

    Config & remove Home

---
+++ BakeryShop/Controllers/BakeryShopController.cs
@@ -0,0 +1,27 @@
+﻿using BakeryShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BakeryShop.Controllers
+{
+    
+
+    public class BakeryShopController : Controller { 
+        private readonly ILogger<BakeryShopController> _logger;
+        public BakeryShopController (ILogger<BakeryShopController> logger)
+        {
+            _logger = logger;
+        }
+    
+        public IActionResult Index()
+        {
+            ViewData["userName"] = "nhutanh";
+            Cake cake = new Cake();
+            cake.Price = 100;   
+            cake.Weight = 100;
+            cake.Description = string.Empty;
+            cake.Name = string.Empty;
+            return View(cake);
+        }
+    }
+}
--- BakeryShop/obj/Release/net8.0/BakeryShop.AssemblyInfo.cs
+++ BakeryShop/obj/Release/net8.0/BakeryShop.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryShop")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4dcc30bdf13e80f0e5fd3ef818d169daf0053fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryShop")]
