6a9fc88bc0b83c132a70df7dbb6dbcf600c2b058
NaGit09 <nhutanhtran806@gmail.com>
2024-11-27 21:53:35 +0700

    update : apply home page into project

--- BakeryShop/obj/Debug/net8.0/BakeryShop.AssemblyInfo.cs
+++ BakeryShop/obj/Debug/net8.0/BakeryShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ac152d55743cf0528400fe724e921d1b173a4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8051eb535dade61ba6a85f6a03aa4cd397d29864")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
