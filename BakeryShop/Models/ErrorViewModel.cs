6ff3cf9083986313bd22887b38106235e0d26155
LeQuocHoaiDuc22130048 <22130048@st.hcmuaf.edu.vn>
2024-11-30 10:58:02 +0700

    Hoàn thành contact và blog

--- BakeryShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BakeryShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
