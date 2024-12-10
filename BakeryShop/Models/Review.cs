fdff213fabeac37dc7b92647731643ed6f9a2dc5
NaGit09 <nhutanhtran806@gmail.com>
2024-12-10 14:56:57 +0700

    extra : add model form bakery shop Api

---
+++ BakeryShop/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bakery_API.Models;
+
+public partial class Review
+{
+    public int ReviewId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public int ProductId { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
