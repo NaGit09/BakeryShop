629d6e9f2e4527c8503a7de5b5101018dad8e715
unknown <22130326@st.hcmuaf.edu.vn>
2024-12-26 12:22:54 +0700

    update Iorder orderservices

---
+++ Bakery_API/Interfaces/IOrder.cs
@@ -0,0 +1,15 @@
+﻿using Bakery_API.DTO;
+using Bakery_API.Models;
+using Bakery_API.Services;
+
+
+namespace Bakery_API.Interfaces
+{
+    public interface IOrder
+
+
+    {
+        List<Order>GetOrdersByDate(DateTime date);
+
+    }
+}
---
+++ Bakery_API/Services/OrderServices.cs
@@ -0,0 +1,32 @@
+﻿using Bakery_API.Models;
+using Bakery_API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery_API.Services
+{
+    public class OrderServices : IOrder
+    {
+        private readonly BakeryShopContext _bakerySqlContext;
+
+        public OrderServices(BakeryShopContext bakerySqlContext)
+        {
+            _bakerySqlContext = bakerySqlContext;
+        }
+
+        // Lấy thông tin đơn hàng theo ngày
+        public List<Order> GetOrdersByDate(DateTime date)
+        {
+            // Truy vấn tất cả các đơn hàng trong ngày
+            var orders = _bakerySqlContext.Orders
+                .Where(order => order.OrderDate == DateOnly.FromDateTime(date))
+                .Include(order => order.User) // Bao gồm thông tin người dùng 
+                .Include(order => order.Payment) // Bao gồm thông tin thanh toán 
+                .Include(order => order.Delivery) // Bao gồm thông tin giao hàng 
+                .ToList();
+
+            return orders;
+        }
+    }
+}
+    
+
