a07dadbfcb9002e06bfaab3a20a7edbac8d8931a
NaGit09 <nhutanhtran806@gmail.com>
2025-01-04 00:24:07 +0700

    fix card item in store

--- Bakery_API/obj/Debug/net8.0/Bakery_API.AssemblyInfo.cs
+++ Bakery_API/obj/Debug/net8.0/Bakery_API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bakery_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d336c3fad87bec7990fd79be008c997d8624e9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7ebd0fabbfc0062cf50be406426f0a0b5d5e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bakery_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bakery_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
