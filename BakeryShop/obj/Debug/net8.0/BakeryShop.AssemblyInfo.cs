34a1e41776561bf319e86948b2f9f29905d087f3
NaGit09 <nhutanhtran806@gmail.com>
2025-01-04 08:28:33 +0700

    complete : tracking

--- BakeryShop/obj/Debug/net8.0/BakeryShop.AssemblyInfo.cs
+++ BakeryShop/obj/Debug/net8.0/BakeryShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7ebd0fabbfc0062cf50be406426f0a0b5d5e3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07dadbfcb9002e06bfaab3a20a7edbac8d8931a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
